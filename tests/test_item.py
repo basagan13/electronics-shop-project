3dbcdd52b5fe8a2184fa00d7a4ffd31688126965
Katya <basagan13@gmail.com>
2023-08-03 22:35:52 +0300

    adding class Item, tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def apple():
+    return Item('apple', 30.0, 5)
+
+
+def test_item(apple):
+    assert apple.calculate_total_price() == 150.0
+
+    Item.pay_rate = 0.8
+    apple.apply_discount()
+    assert apple.calculate_total_price() == 120.0
